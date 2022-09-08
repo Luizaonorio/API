9885a6e56cb170ef79bebe9459538c774fb7b624
luizaonorio <luiza.onorio@hotmail.com>
2022-09-08 07:08:17 -0300

    date formatted

--- models/People.js
+++ models/People.js
@@ -1,21 +1,38 @@
 import mongoose from "mongoose";
 
+function birthday(birth) {
+    let birthday = new Date(birth);
+    let inDate = new Date();
+    let age = Math.floor((inDate - birthday) / 31536000000);
+
+    if (age > 17) {
+        return true;
+    } else {
+        return false;
+    };
+}
+
+function changeDate(date) {
+    let inDate = new Date(date);
+    return inDate.toLocaleDateString();
+}
+
 const peopleSchema = new mongoose.Schema(
     {
         id: { type: String },
         name: { type: String, validate: /[A-zÀ-ú\s]+$/, required: true },
-        cpf: { type: String, validate: /^[0-9]*$/, minLength: 11, maxLength: 11, required: true },
-        birthDate: { type: String, required: true },
-        email: { type: String, validate: /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/, required: true},
-        password: { type: String, select: false, minLength: 6, required: true },
-        address: { type: String, required: true },
-        number: { type: String, required: true },
+        cpf: { type: String, validate: /^[0-9]*$/, minlength: 11, maxlength: 11, required: true },
+        birthDate: { type: String, set: date => changeDate(date), required: true, validate: [birthday, "18"] },
+        email: { type: String, validate: /^[\w-.]+@([\w-]+.)+[\w-]{2,4}$/, required: true },
+        password: { type: String, minlength: 6, required: true },
+        address: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
+        number: { type: String, validate: /^[0-9]*$/, required: true },
         complement: { type: String, required: true },
-        city: { type: String, required: true },
-        state: { type: String, required: true },
-        country: { type: String, required: true },
-        zipCode: { type: String, minLength: 8, maxLength: 8, required: true }
-    },
+        city: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
+        state: { type: String, validate: /[A-Z]/, required: true },
+        country: { type: String, validate: /[A-zÀ-ú\s]/, required: true },
+        zipCode: { type: String, validate: /^[0-9]*$/, minlength: 8, maxlength: 8, required: true }
+      },
     {
         versionKey: false
     }
--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,10 @@ const routes = (app) => {
         res.status(200).send({ Project: "Back-end" });
     })
 
+    // app.route('/api/v1/user').get((req, res) => {
+    //     res.status(200).send('To see the users you must use /api/v1/user?page=num&limit=num')
+    // })
+
     app.use(express.json(), people)
 }
 
