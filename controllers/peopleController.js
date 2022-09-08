4a1a16702c9a2b4d6576984b029c050843bc46e4
luizaonorio <luiza.onorio@hotmail.com>
2022-09-08 00:33:47 -0300

    adding person

--- controllers/peopleController.js
+++ controllers/peopleController.js
@@ -1,76 +1,75 @@
-import person from "../models/People.js";
+import person from '../models/People.js'
 
 class PeopleController {
+  static listPeople = (req, res) => {
+    person.find((err, person) => {
+      res.status(200).json(person)
+    })
+  }
 
-    static listPeople = (req, res) => {
+  static listPeopleById = (req, res) => {
+    const id = req.params.id
+    person.findById(id, (err, person) => {
+      if (err) {
+        res
+          .status(404)
+          .send({ message: `${err.message} - Person id not found` })
+      } else {
+        const peopleStrip = { ...person['_doc'] }
+        delete peopleStrip.password
+        res.status(200).send(peopleStrip)
+      }
+    })
+  }
 
-      person.find((err, person) => {
+  static listPeopleByName = (req, res) => {
+    const name = req.params.name
 
-            person = person.map(people => {
-                const peopleStrip = { ...people['_doc'] };
-                delete peopleStrip.password;
-                return peopleStrip;
-            })
+    person.find({ name: { $regex: name } }, {}, (err, person) => {
+      if (err) {
+        res
+          .status(404)
+          .send({ message: `${err.message} - Person name not found` })
+      } else {
+        const peopleStrip = { ...person['_doc'] }
+        delete peopleStrip.password
+        res.status(200).send(peopleStrip)
+      }
+    })
+  }
 
-            res.status(200).json(person);
-        })
-    }
+  static registerPeople = (req, res) => {
+    let people = new person(req.body)
+    people.save((err, people) => {
+      err
+        ? res
+            .status(500)
+            .send({ message: `Register Person Falied- ${err.message}` })
+        : res.status(201).send(people.toJSON())
+    })
+  }
 
-    static listPeopleById = (req, res) => {
-        const id = req.params.id;
-        person.findById(id, (err, person) => {
-            if (err) {
-                res.status(404).send({ message: `${err.message} - Person id not found` });
-            } else {
-                const peopleStrip = { ...person['_doc'] };
-                delete peopleStrip.password;
-                res.status(200).send(peopleStrip);
-            }
-        })
-    }
+  static updatePeople = (req, res) => {
+    const id = req.params.id
+    person.findByIdAndUpdate(id, { $set: req.body }, err => {
+      if (!err) {
+        res.status(200).send({ message: 'Person updated successfully' })
+      } else {
+        res.status(404).send({ message: `${err.message} - Person Not Found` })
+      }
+    })
+  }
 
-    static listPeopleByName = (req, res) => {
-        const name = req.params.name;
-
-        person.find({'name': {$regex: name}}, {}, (err, person) => {
-            if (err) {
-                res.status(404).send({ message: `${err.message} - Person name not found` });
-            } else {
-                const peopleStrip = { ...person['_doc'] };
-                delete peopleStrip.password;
-                res.status(200).send(peopleStrip);
-            }
-        })
-    }
-
-    static registerPeople = (req, res) => {
-        let people = new (req.body);
-        people.save((err, people) => {
-            err ? res.status(500).send({ message: `Register Person Falied- ${err.message}` }) : res.status(201).send(people.toJSON())
-        })
-    }
-
-    static updatePeople = (req, res) => {
-        const id = req.params.id;
-        person.findByIdAndUpdate(id, { $set: req.body }, (err) => {
-            if (!err) {
-                res.status(200).send({ message: "Person updated successfully" });
-            } else {
-                res.status(404).send({ message: `${err.message} - Person Not Found` })
-            }
-        })
-    }
-
-    static deletePeople = (req, res) => {
-        const id = req.params.id;
-        person.findByIdAndRemove(id, (err) => {
-            if (!err) {
-                res.status(204).send({ message: "Person successfully deleted" });
-            } else {
-                res.status(404);
-            }
-        })
-    }
+  static deletePeople = (req, res) => {
+    const id = req.params.id
+    person.findByIdAndRemove(id, err => {
+      if (!err) {
+        res.status(204).send({ message: 'Person successfully deleted' })
+      } else {
+        res.status(404)
+      }
+    })
+  }
 }
 
-export default PeopleController;
+export default PeopleController
